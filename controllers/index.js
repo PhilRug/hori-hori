8aea7771853cebe52c4952870c589cd9b8186406
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-05-01 21:13:01 -0400

    added homeroute to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoute = require('./homeroute')
 
+router.use('/', homeRoute)
 router.use('/api', apiRoutes);
 
 module.exports = router;
