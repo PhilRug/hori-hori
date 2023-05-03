c5b486719bb3175563198b54a2de41dd794ba27b
PhilRug <ruggiero.pj@gmail.com>
2023-05-02 20:26:20 -0400

    routes fixed and workable homepage

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(routes);
 const sess = {
   secret: process.env.SECRET,
   cookie: {},
@@ -23,8 +22,11 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-
+app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
